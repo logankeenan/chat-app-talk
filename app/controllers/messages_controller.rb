b5ee144e0917d6cab713e953ad7af5120b776b4e
Logan Keenan <d.logankeenan@gmail.com>
2018-09-17 15:41:43 -0500

    add embedded chat message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -29,6 +29,7 @@ class MessagesController < ApplicationController
     respond_to do |format|
       if @message.save
         format.html { redirect_to @message, notice: 'Message was successfully created.' }
+        format.js
         format.json { render :show, status: :created, location: @message }
       else
         format.html { render :new }
