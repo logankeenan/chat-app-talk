4c6a626d253835488f9d68c22ece58aea3b84a45
Logan Keenan <d.logankeenan@gmail.com>
2018-09-17 19:12:56 -0500

    add user and message reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_230031) do
+ActiveRecord::Schema.define(version: 2018_09_17_235846) do
 
   create_table "chat_rooms", force: :cascade do |t|
     t.string "name"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2018_09_17_230031) do
     t.integer "chat_room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", default: 1
     t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
